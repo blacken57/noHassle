7c5eff5c881002db85f81a4bd8d0ef0e120947b8
Abhishek Roy <abhishekriju57@gmail.com>
2020-07-07 00:14:12 +0530

    Add files via upload

--- index.py
+++ index.py
@@ -1,10 +1,20 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request, render_template
 import create_face
+import svmcamera
+import joblib
 app = Flask(__name__)
 
-@app.route('/success/<name>')
-def success(name):
-   return 'welcome %s' % name
+name = ""
+index = -1
+
+@app.route('/buy/<name>')
+def buy(name):
+   dict = joblib.load("catalogue")
+   res = []
+   for a in dict:
+      res.append(a)
+   h = svmcamera.person()
+   return render_template('money.html', colours=res, name = h[0])
 
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
@@ -12,11 +22,15 @@ def login():
       user = request.form['nm']
       create_face.create_face_website(user)
       
-      return redirect(url_for('success',name = user))
+      return redirect(url_for('buy',name = user))
    else:
       user = request.args.get('nm')
       create_face.create_face_website(user)
       return redirect(url_for('success',name = user))
+
+
+
+
 
 if __name__ == '__main__':
    app.run(debug = True)
