1d09553f32124c4825b46767865cd4d1f5c18d0f
blacken57 <abhishekriju57@gmail.com>
2020-07-07 11:40:34 +0530

    Added Buy

--- index.py
+++ index.py
@@ -7,24 +7,31 @@ app = Flask(__name__)
 name = ""
 index = -1
 
-@app.route('/buy/<name>')
-def buy(name):
+@app.route('/buy',methods = ['POST'])
+def buy():
    dict = joblib.load("catalogue")
    res = []
    for a in dict:
       res.append(a)
-   h = svmcamera.person()
-   name = h[0]
-   index = h[1]
-   return render_template('money.html', colours=res, name = h[0])
+   if request.method == 'POST':
+     if request.form['ques'] == 'Yes':
+      
+      return render_template('products.html')
+     elif request.form['ques'] == 'No':
+      return render_template('login.html',methods = ['POST','GET'])
 
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
    if request.method == 'POST':
       user = request.form['nm']
-      create_face.create_face_website(user)
-      
-      return redirect(url_for('buy',name = user))
+      #create_face.create_face_website(user)      
+      #return redirect(url_for('buy',name = user))
+      dict = joblib.load("catalogue")
+      res = []
+      for a in dict:
+         res.append(a)
+      names = svmcamera.person()
+      return render_template('buy.html',person=names[0])
    else:
       user = request.args.get('nm')
       create_face.create_face_website(user)
